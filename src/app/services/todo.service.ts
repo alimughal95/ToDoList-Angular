ac466bbfec6153a0b836cf3497c764fc726f4c58
ali hassan <ali.mughal.1895@gmail.com>
2020-11-01 22:27:08 +0500

    First Commit

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import {  Todo } from "../models/Todo"
+import { Observable } from 'rxjs';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'content-Type': "application/json"
+  })
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  todosUrl:string = "https://jsonplaceholder.typicode.com/todos";
+  todosLimit = "?_limit=5"
+  constructor(private http:HttpClient) { }
+
+// Get TODOS
+  getTodos():Observable<Todo[]>{
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+  }
+
+  //Toggles Completed
+
+  toggleCompleted(todo:Todo):Observable<any>{
+    const url = `${this.todosUrl}/${todo.id}`
+    return this.http.put(url,todo, httpOptions);
+  }
+
+  // Delete Toggle 
+
+  deleteTodo(todo:Todo):Observable<Todo>{
+    const url = `${this.todosUrl}/${todo.id}`
+    return this.http.delete<Todo>(url, httpOptions);
+  }
+
+// Add Todo 
+
+addTodo(todo:Todo):Observable<Todo> {
+  return this.http.post<Todo>(this.todosUrl, todo, httpOptions)
+}
+
+}
+ 
